b12336f8234e3a7dcf5e4ed70741a61a942acac0
dongtaoy <dongt.yu@gmail.com>
2015-04-27 22:28:15 +1000

    add detail page of survey and todo edit page

--- survey/views.py
+++ survey/views.py
@@ -1,10 +1,19 @@
 __author__ = 'dongtao'
 from django.views.generic.edit import CreateView
+from django.views.generic import DetailView
 from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import redirect
 from django.db.transaction import atomic
-
+from core.models import Survey
 from core.forms import SurveyFrom
+
+
+class SurveryDetailView(DetailView):
+    model = Survey
+    template_name = "survey/survey.detail.html"
+    pk_url_kwarg = 'survey'
+
+
 
 
 class SurveyCreateView(CreateView):
@@ -17,4 +26,4 @@ class SurveyCreateView(CreateView):
             survey = form.save(commit=False)
             survey.creator = self.request.user
             survey.save()
-        return redirect(self.success_url)
+        return redirect(reverse_lazy("survey.detail", kwargs={"survey": survey.id}))
