53c737b2eec600cefd8239805736620923a2b0c7
georgecai904 <hongdou20100830>
2015-04-28 22:00:03 +1000

    builder

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
+from django.views.generic.base import TemplateView
 from core.views import DashboardView
 
 urlpatterns = patterns(
@@ -8,14 +9,14 @@ urlpatterns = patterns(
 
     url(r'^$', 'core.views.home', name='home'),
 
-    url(r'^dashboard/$', login_required(DashboardView.as_view()), name='dashboard')
+    url(r'^dashboard/$', login_required(DashboardView.as_view()), name='dashboard'),
 
-
+    url(r'^403$', login_required(TemplateView.as_view(template_name='common/403.html'))),
 
     # common page testing
     # url(r'^403$', login_required(TemplateView.as_view(template_name='common/403.html'))),
-    # url(r'^404$', login_required(TemplateView.as_view(template_name='common/404.html'))),
-    # url(r'^500$', login_required(TemplateView.as_view(template_name='common/500.html'))),
-    # url(r'^delconfirm/$', login_required(TemplateView.as_view(template_name='common/delete.confirmation.html')), name='delete.confirmation'),
+    url(r'^404$', login_required(TemplateView.as_view(template_name='common/404.html'))),
+    url(r'^500$', login_required(TemplateView.as_view(template_name='common/500.html'))),
+    url(r'^delconfirm/$', login_required(TemplateView.as_view(template_name='common/delete.confirmation.html')), name='delete.confirmation'),
 )
 
