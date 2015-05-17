4fbe360d1e12e2b3da81415f4e8e472c6e12c4d3
georgecai904 <hongdou20100830>
2015-05-17 22:57:05 +1000

    Survey Collector

--- static/assets/survey/js/survey.collect.js
+++ static/assets/survey/js/survey.collect.js
@@ -43,12 +43,11 @@ var surveyCollect = (function () {
             var client = new ZeroClipboard($(this));
             client.on("ready", function (readyEvent) {
                 // alert( "ZeroClipboard SWF is ready!" );
-
                 client.on("aftercopy", function (event) {
                     // `this` === `client`
                     // `event.target` === the element that was clicked
                     //event.target.style.display = "none";
-                    alert("Copied text to clipboard: " + event.data["text/plain"]);
+                    alert("Copied: " + event.data["text/plain"]);
                 });
             });
         });
